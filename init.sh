64bbb4a991d3d592bca8435d9c6d211c917723a4
Chen Zaichuang <1158400745@qq.com>
2020-04-17 00:49:08 +0800

    fix SCRIPT_PATH value

--- init.sh
+++ init.sh
@@ -1,4 +1,4 @@
-SCRIPT_PATH=$(pwd)
+SCRIPT_PATH="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 ${SCRIPT_PATH}/init_submodule.sh ngrok_ssh_client git@github.com:ChenZaichuang/Ngrok-SSH-Client.git
 ${SCRIPT_PATH}/init_submodule.sh configuration git@github.com:ChenZaichuang/Configuration.git
 pip3 install -r ${SCRIPT_PATH}/requirements.txt
