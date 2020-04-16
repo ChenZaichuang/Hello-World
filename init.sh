dbb5a095fc06ba18baef55917b68385405ea3155
Chen Zaichuang <1158400745@qq.com>
2020-04-17 00:30:49 +0800

    use pip3 instead of pip

--- init.sh
+++ init.sh
@@ -1,5 +1,5 @@
 SCRIPT_PATH=$(pwd)
 ${SCRIPT_PATH}/init_submodule.sh ngrok_ssh_client git@github.com:ChenZaichuang/Ngrok-SSH-Client.git
 ${SCRIPT_PATH}/init_submodule.sh configuration git@github.com:ChenZaichuang/Configuration.git
-pip install -r ${SCRIPT_PATH}/requirements.txt
+pip3 install -r ${SCRIPT_PATH}/requirements.txt
 find ${SCRIPT_PATH} -type d -empty -delete
