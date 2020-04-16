3fb937dafea5656950a5475044e50b5fbbae3332
Chen Zaichuang <1158400745@qq.com>
2020-04-17 00:33:48 +0800

    Use sudo instead of sudo su.

--- set_start_with_system.sh
+++ set_start_with_system.sh
@@ -1,5 +1,4 @@
-sudo su
-echo -e '[Unit]\nDescription=/etc/rc.local Compatibility\nConditionPathExists=/etc/rc.local\n \n[Service]\nType=forking\nExecStart=/etc/rc.local start\nTimeoutSec=0\nStandardOutput=tty\nRemainAfterExit=yes\nSysVStartPriority=99\n \n[Install]\nWantedBy=multi-user.target\n' > /etc/systemd/system/rc-local.service
-echo -e '#!/bin/sh -e \n su firefly -c '"'python3 /home/firefly/Documents/Ngrok-SSH/start_ngrok.py' \n exit 0" > /etc/rc.local
-chmod +x /etc/rc.local
-systemctl enable rc-local
+sudo echo -e '[Unit]\nDescription=/etc/rc.local Compatibility\nConditionPathExists=/etc/rc.local\n \n[Service]\nType=forking\nExecStart=/etc/rc.local start\nTimeoutSec=0\nStandardOutput=tty\nRemainAfterExit=yes\nSysVStartPriority=99\n \n[Install]\nWantedBy=multi-user.target\n' > /etc/systemd/system/rc-local.service
+sudo echo -e '#!/bin/sh -e \n su firefly -c '"'python3 /home/firefly/Documents/Ngrok-SSH/start_ngrok.py' \n exit 0" > /etc/rc.local
+sudo chmod +x /etc/rc.local
+sudo systemctl enable rc-local
