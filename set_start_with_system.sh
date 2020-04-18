e5a44570e93def2f42ce099ffb9eebd9238c6b66
Chen Zaichuang <1158400745@qq.com>
2020-04-18 16:18:28 +0800

    Polish set_start_with_system.sh to run without switching to root account.

--- set_start_with_system.sh
+++ set_start_with_system.sh
@@ -1,5 +1,8 @@
-#run with sudo
-echo -e '[Unit]\nDescription=/etc/rc.local Compatibility\nConditionPathExists=/etc/rc.local\n \n[Service]\nType=forking\nExecStart=/etc/rc.local start\nTimeoutSec=0\nStandardOutput=tty\nRemainAfterExit=yes\nSysVStartPriority=99\n \n[Install]\nWantedBy=multi-user.target\n' > /etc/systemd/system/rc-local.service
-echo -e '#!/bin/sh -e \n su firefly -c '"'python3 /home/firefly/Documents/Ngrok-SSH-Server/start_ngrok.py' \n exit 0" > /etc/rc.local
-chmod +x /etc/rc.local
-systemctl enable rc-local
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+
+command=$'echo -e \'[Unit]\nDescription=/etc/rc.local Compatibility\nConditionPathExists=/etc/rc.local\n \n[Service]\nType=forking\nExecStart=/etc/rc.local start\nTimeoutSec=0\nStandardOutput=tty\nRemainAfterExit=yes\nSysVStartPriority=99\n \n[Install]\nWantedBy=multi-user.target\n\'> /etc/systemd/system/rc-local.service && '
+command+=$'echo -e \'#!/bin/sh -e \nsu firefly -c \'\"\'python3 '${DIR}$'/Ngrok-SSH-Server/start_ngrok.py\' \nexit 0\" > /etc/rc.local && '
+command+=$'chmod +x /etc/rc.local && '
+command+=$'systemctl enable rc-local'
+
+su -c "${command}" root
