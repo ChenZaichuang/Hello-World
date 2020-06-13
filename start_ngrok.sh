eb3792c6c5765ffa92074e62e1ea3aec92a733b6
Chen Zaichuang <1158400745@qq.com>
2020-06-13 22:10:13 +0800

    Fix bash script.

--- start_ngrok.sh
+++ start_ngrok.sh
@@ -1,7 +1,10 @@
 #!/usr/bin/env bash
 set -e
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 cd "$( dirname "${BASH_SOURCE[0]}" )"
 pwd
 ls
+echo 'DIR:'
+echo ${DIR}
 #bash init.sh
 #python3 start_ngrok.py
