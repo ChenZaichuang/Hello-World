f5c6d74800e11f99073c2b712a0064f3a88ee14d
Chen Zaichuang <1158400745@qq.com>
2020-06-13 22:39:56 +0800

    Fix bash script.

--- set_start_with_system.sh
+++ set_start_with_system.sh
@@ -1,7 +1,7 @@
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
 command=$'echo -e \'[Unit]\nDescription=/etc/rc.local Compatibility\nConditionPathExists=/etc/rc.local\n \n[Service]\nType=forking\nExecStart=/etc/rc.local start\nTimeoutSec=0\nStandardOutput=tty\nRemainAfterExit=yes\nSysVStartPriority=99\n \n[Install]\nWantedBy=multi-user.target\n\'> /etc/systemd/system/rc-local.service && '
-command+=$'echo -e \'#!/bin/sh -e \nsu firefly -c \'\"\'sh '${DIR}$'/start_ngrok.sh\' \nexit 0\" > /etc/rc.local && '
+command+=$'echo -e \'#!/bin/bash -e \nsu firefly -c \'\"\'bash '${DIR}$'/start_ngrok.sh\' \nexit 0\" > /etc/rc.local && '
 command+=$'chmod +x /etc/rc.local && '
 command+=$'systemctl enable rc-local'
 
--- start_ngrok.sh
+++ start_ngrok.sh
@@ -1,10 +1,4 @@
 #!/usr/bin/env bash
 set -e
-DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 cd "$( dirname "${BASH_SOURCE[0]}" )"
-pwd
-ls
-echo 'DIR:'
-echo ${DIR}
-#bash init.sh
-#python3 start_ngrok.py
+python3 start_ngrok.py
